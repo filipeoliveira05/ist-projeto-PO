69c0ee257f68a7866edcb17f13a87feca38aeac3
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-07 23:09:27 +0100

    FILIPE - docstring no DuplicateAnimalKeyException

--- hva-app/src/hva/app/exceptions/DuplicateAnimalKeyException.java
+++ hva-app/src/hva/app/exceptions/DuplicateAnimalKeyException.java
@@ -6,8 +6,10 @@ import java.io.Serial;
 
 public class DuplicateAnimalKeyException extends CommandException {
 	@Serial
+	/** Serial number for serialization. */
 	private static final long serialVersionUID = 202407081733L;
 
+	/** @param id the duplicated key */
 	public DuplicateAnimalKeyException(String id) {
 		super(Message.duplicateAnimalKey(id));
 	}
