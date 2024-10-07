d8812559f88195f2b0c1d7d59ade85a1009f9a50
Eduardo Fernandes <eduardo.x.fernandes@tecnico.ulisboa.pt>
2024-10-07 17:19:26 +0100

    EDU- parentises

--- hva-app/src/hva/app/vaccine/Message.java
+++ hva-app/src/hva/app/vaccine/Message.java
@@ -3,7 +3,7 @@ package hva.app.vaccine;
 public interface Message {
 
     static String wrongVaccine(String vaccineKey, String animalKey) {
-        return "A vacina '" + vaccineKey + "' não é apropiada para o animal '" + animalKey + "'.";
+        return "A vacina '" + vaccineKey + "' não é apropiada para o animal '" + animalKey + "'.";}
 
     static String vaccineRegistered(String vaccineName) {
         return "Vacina " + vaccineName + " registada com sucesso!";
