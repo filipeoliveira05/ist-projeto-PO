4d7dac7fa1d445fda45f9c2f3ee97998f1a9c295
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-07 23:25:53 +0100

    FILIPE - consegui registar um animal novo no terminal

--- hva-app/src/hva/app/animal/DoRegisterAnimal.java
+++ hva-app/src/hva/app/animal/DoRegisterAnimal.java
@@ -1,43 +1,36 @@
 package hva.app.animal;
 
+import hva.Hotel;
+import hva.app.exceptions.DuplicateAnimalKeyException;
+
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
 
-import java.text.Normalizer.Form;
 
-import hva.Hotel;
-import hva.exceptions.DuplicateAnimalKeyException;
+
 
 class DoRegisterAnimal extends Command<Hotel> {
 
     DoRegisterAnimal(Hotel receiver) {
         super(Label.REGISTER_ANIMAL, receiver);
+        addStringField("id", Prompt.animalKey());
+        addStringField("name", Prompt.animalName());
+        addStringField("idSpecies", Prompt.speciesKey());
+        addStringField("idHabitat", Prompt.habitatKey());
     }
 
     @Override
     protected final void execute() throws CommandException {
-        /*String animalId = Form.requestString(Prompt.animalKey());
-
-        // Verifica se o animal já existe
         try {
-            _receiver.checkDuplicateAnimal(animalId);
-        } catch (DuplicateAnimalKeyException e) {
-            throw e;
-        }
-
-        String animalName = Form.requestString(Prompt.animalName());
-        String speciesId = Form.requestString(Prompt.speciesKey());
-
-        // Se a espécie não existir, solicita o nome da nova espécie e regista
-        if (!_receiver.speciesExists(speciesId)) {
-            String speciesName = Form.requestString(Prompt.speciesName());
-            _receiver.registerSpecies(speciesId, speciesName);
-        }
-
-        String habitatId = Form.requestString("Insira o identificador do habitat:");
-        _receiver.registerAnimal(animalId, animalName, speciesId, habitatId);
-
-        _display.popup(Message.animalRegistered(animalName));*/
+            _receiver.registerAnimal(
+                    stringField("id"),
+                    stringField("name"),
+                    stringField("idSpecies"),
+                    stringField("idHabitat")
+            );
+          } catch (hva.exceptions.DuplicateAnimalKeyException e) {
+            throw new DuplicateAnimalKeyException(e.getKey());
+          }
     }
 }
 
