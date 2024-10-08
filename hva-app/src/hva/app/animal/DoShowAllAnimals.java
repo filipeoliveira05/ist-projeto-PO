9b1ed11fc69779ace721b7d6d1fdd9d62cda2f74
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-08 22:07:35 +0100

    FILIPE - DoShowAllAnimals primeira tentativa

--- hva-app/src/hva/app/animal/DoShowAllAnimals.java
+++ hva-app/src/hva/app/animal/DoShowAllAnimals.java
@@ -12,11 +12,7 @@ class DoShowAllAnimals extends Command<Hotel> {
 
     @Override
     protected final void execute() {
-        //FIXME implement command
-        /*
-        //adicionado por mim
-        _receiver.showAllAnimals();
-        */
+        _display.popup(_receiver.getAllAnimals());
     }
 
 }
