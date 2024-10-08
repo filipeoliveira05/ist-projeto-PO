40fe4ee87a5458de6efd5330693519a977bce776
Eduardo Fernandes <eduardo.x.fernandes@tecnico.ulisboa.pt>
2024-10-08 18:27:19 +0100

    erro estupido

--- hva-app/src/hva/app/exceptions/SaveCommandException.java
+++ hva-app/src/hva/app/exceptions/SaveCommandException.java
@@ -1,6 +1,6 @@
 package hva.app.exceptions;
 
-import.pt.tecnico.uilib.menus.CommandException;
+import pt.tecnico.uilib.menus.CommandException;
 
 public class SaveCommandException {
     public SaveCommandException(String message){
