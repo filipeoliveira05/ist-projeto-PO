3f944d9fc8539b33717f98349f87b3f595aee31d
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-09 17:51:50 +0100

    FILIPE - DoRegisterHabitat primeira tentativa

--- hva-app/src/hva/app/habitat/DoRegisterHabitat.java
+++ hva-app/src/hva/app/habitat/DoRegisterHabitat.java
@@ -1,36 +1,30 @@
 package hva.app.habitat;
 
-import java.text.Normalizer.Form;
-
 import hva.Hotel;
 import hva.app.exceptions.DuplicateHabitatKeyException;
+
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME import other classes if needed
 
 class DoRegisterHabitat extends Command<Hotel> {
 
     DoRegisterHabitat(Hotel receiver) {
         super(Label.REGISTER_HABITAT, receiver);
+        addStringField("id", Prompt.habitatKey());
+        addStringField("name", Prompt.habitatName());
+        addStringField("area", Prompt.habitatArea());
     }
 
     @Override
     protected void execute() throws CommandException {
-        /*
-        String habitatId = Form.requestString(Prompt.habitatKey());
-
-        // Verifica duplicidade de habitat
         try {
-            _receiver.checkDuplicateHabitat(habitatId);
-        } catch (DuplicateHabitatKeyException e) {
-            throw e;
-        }
-
-        String habitatName = Form.requestString(Prompt.habitatName());
-        int habitatArea = Form.requestInteger(Prompt.habitatArea());
-
-        _receiver.registerHabitat(habitatId, habitatName, habitatArea);  // Registra o habitat
-        _display.popup(Message.habitatRegistered(habitatName));
-        */
+            _receiver.registerHabitat(
+                    stringField("id"),
+                    stringField("name"),
+                    stringField("area")
+            );
+          } catch (hva.exceptions.DuplicateHabitatKeyException e) {
+            throw new DuplicateHabitatKeyException(e.getKey());
+          }
     }
 }
