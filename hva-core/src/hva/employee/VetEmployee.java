feb05402c7ec399fcfd2e4d001ca3a020b28da58
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-10 18:01:01 +0100

    FILIPE - class VetEmployee primeira tentativa

---
+++ hva-core/src/hva/employee/VetEmployee.java
@@ -0,0 +1,34 @@
+package hva.employee;
+
+import hva.exceptions.DuplicateEmployeeKeyException;
+import hva.species.Species;
+
+import java.io.Serial;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.HashMap;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class VetEmployee extends Employee{
+
+    /**
+    * Serial number for serialization.
+    */
+    @Serial
+    private static final long serialVersionUID = 202410081525L;
+
+    private final String type;
+    private Map<String, Species> responsabilitiesSpecies;
+    
+    public VetEmployee(String id, String name) {
+        super(id, name);
+        this.type = "VET";
+        this.responsabilitiesSpecies = new HashMap<>();
+    }
+
+    public void addSpecies(Species species){
+        responsabilitiesSpecies.put(species.getId(), species);
+    }
+}
