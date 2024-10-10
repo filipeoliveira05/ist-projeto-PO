901d346e16dbe269f3186170099bf3dec37041db
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-10 18:00:44 +0100

    FILIPE - class Employee primeira tentativa

---
+++ hva-core/src/hva/employee/Employee.java
@@ -0,0 +1,54 @@
+package hva.employee;
+
+import hva.exceptions.DuplicateEmployeeKeyException;
+import hva.exceptions.NoResponsibilityException;
+import hva.util.NaturalTextComparator;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
+
+public class Employee implements Serializable{
+    
+    /**
+    * Serial number for serialization.
+    */
+    @Serial
+    private static final long serialVersionUID = 202410081520L;
+
+
+    private static final Comparator<String> ID_COMPARATOR 
+            = new NaturalTextComparator();
+    private String id;
+    private String name;
+    
+    
+    public Employee(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+
+    //Getter for id
+    public String getId() {
+        return this.id;
+    }
+
+    // Getter for name
+    public String getName() {
+        return this.name;
+    }
+
+}   
