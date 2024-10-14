49d11ac38db56fa2ebefd39b8665cf5fae8da100
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-14 20:58:50 +0100

    FILIPE - forma de dar sort a strings alterada

--- hva-core/src/hva/util/SortingUtils.java
+++ hva-core/src/hva/util/SortingUtils.java
@@ -7,23 +7,17 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class SortingUtils {
-
     /**
      * Utility function to get a case-insensitive sorted collection from a map.
      *
-     * @param <K> the type of the map's key
      * @param <V> the type of the map's value
-     * @param map The map to be sorted
-     * @return A collection of values sorted by keys in case-insensitive manner
+     * @param map The map to be sorted, where keys are strings
+     * @return A collection of values sorted by keys in a case-insensitive manner
      */
-    public static <K, V> Collection<V> getSortedCollection(Map<K, V> map) {
-        Map<String, V> sortedMap = new TreeMap<>(new Comparator<String>() {
-            public int compare(String key1, String key2) {
-                return key1.toLowerCase().compareTo(key2.toLowerCase());
-            }
-        });
-
-        map.forEach((key, value) -> sortedMap.put(key.toString(), value));
+    public <V> Collection<V> getSortedCollection(Map<String, V> map) {
+        Map<String, V> sortedMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
+        
+        sortedMap.putAll(map);
         
         return Collections.unmodifiableCollection(sortedMap.values());
     }
