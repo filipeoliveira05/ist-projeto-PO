43fc7a43ec51ef2b15463d81f241fe3ee875c464
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-15 21:44:17 +0100

    FILIPE - addAnimal e addEmployee na classe Habitat

--- hva-core/src/hva/habitat/Habitat.java
+++ hva-core/src/hva/habitat/Habitat.java
@@ -1,6 +1,8 @@
 package hva.habitat;
 
 import hva.tree.Tree;
+import hva.animal.Animal;
+import hva.employee.Employee;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -19,6 +21,8 @@ public class Habitat implements Serializable{
     private String name;
     private int area;
     private Map<String, Tree> trees;
+    private Map<String, Animal> animals;
+    private Map<String, Employee> employees;
     
     
     public Habitat(String id, String name, int area) {
@@ -26,11 +30,38 @@ public class Habitat implements Serializable{
         this.name = name;
         this.area = area;
         this.trees = new HashMap<>();
+        this.animals = new HashMap<>();
+        this.employees = new HashMap<>();
     }
 
 
+    /**
+    * Adds a new tree to the map of trees in the habitat
+    * 
+    * @param tree The {@link Tree} object to be added.
+    */
     public void addTree(Tree tree){
         trees.put(tree.getId(), tree);
+    }
+
+
+    /**
+    * Adds a new animal to the map of animals in the habitat
+    * 
+    * @param animal The {@link Animal} object to be added.
+    */
+    public void addAnimal(Animal animal){
+        animals.put(animal.getId(), animal);
+    }
+
+
+    /**
+    * Adds a new employee to the map of employees in the habitat
+    * 
+    * @param employee The {@link Employee} object to be added.
+    */
+    public void addEmployee(Employee employee){
+        employees.put(employee.getId(), employee);
     }
 
 
@@ -52,4 +83,15 @@ public class Habitat implements Serializable{
     public int getNumberTrees () {
         return trees.size();
     }
+
+
+    /**
+    * Updates the area of the habitat.
+    * 
+    * @param newArea The new area to set for the habitat.
+    */
+    public void setArea(int newArea) {
+        this.area = newArea;
+    }
+
 }
