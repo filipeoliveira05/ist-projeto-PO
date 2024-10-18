f66ef0ca59effe6f63ced16e23d1a0f3edf3716f
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-18 20:12:08 +0100

    FILIPE - DoRegisterEmployee primeira tentativa

--- hva-app/src/hva/app/employee/DoRegisterEmployee.java
+++ hva-app/src/hva/app/employee/DoRegisterEmployee.java
@@ -1,19 +1,40 @@
 package hva.app.employee;
 
-import java.text.Normalizer;
+import hva.Hotel;
+
+import hva.app.exceptions.DuplicateEmployeeKeyException;
+
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-import hva.Hotel;
-import hva.exceptions.DuplicateEmployeeKeyException;
+
+import java.text.Normalizer;
 
 class DoRegisterEmployee extends Command<Hotel> {
 
     DoRegisterEmployee(Hotel receiver) {
         super(Label.REGISTER_EMPLOYEE, receiver);
+        addStringField("id", Prompt.employeeKey());
+        addStringField("name", Prompt.employeeName());
+        addStringField("type", Prompt.employeeType());
     }
 
     @Override
     protected void execute() throws CommandException {
-        //FIXME implement command
+        //FIXME VET/TRT únicos inputs válidos
+        String type;
+
+        do {
+            type = stringField("type").toUpperCase();
+        } while (!type.equals("VET") && !type.equals("TRT"));
+
+        try {
+            if (type.equals("VET")) {
+                _receiver.registerVet(stringField("id"), stringField("name"));
+            } else if (type.equals("TRT")) {
+                _receiver.registerCaretaker(stringField("id"), stringField("name"));
+            }
+        } catch (hva.exceptions.DuplicateEmployeeKeyException e) {
+            throw new DuplicateEmployeeKeyException(e.getKey());
+        }
     }
 }
