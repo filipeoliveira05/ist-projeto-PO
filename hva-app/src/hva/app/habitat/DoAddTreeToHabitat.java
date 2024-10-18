1643c16cb93ab7905fb664f622ffb264bffdc9fd
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-18 12:56:56 +0100

    FILIPE - DoAddTreeToHabitat primeira tentativa

--- hva-app/src/hva/app/habitat/DoAddTreeToHabitat.java
+++ hva-app/src/hva/app/habitat/DoAddTreeToHabitat.java
@@ -1,20 +1,44 @@
 package hva.app.habitat;
 
-import java.text.Normalizer.Form;
-
 import hva.Hotel;
+import hva.exceptions.UnknownHabitatException;
+
+import hva.app.exceptions.DuplicateTreeKeyException;
+import hva.app.exceptions.UnknownHabitatKeyException;
+
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME import other classes if needed
+
+import java.text.Normalizer.Form;
 
 class DoAddTreeToHabitat extends Command<Hotel> {
-
+    //FIXME PERENE/CADUCA únicos inputs válidos
     DoAddTreeToHabitat(Hotel receiver) {
         super(Label.ADD_TREE_TO_HABITAT, receiver);
+        addStringField("id", Prompt.habitatKey());
+        addStringField("idTree", Prompt.treeKey());
+        addStringField("nameTree", Prompt.treeName());
+        addIntegerField("ageTree", Prompt.treeAge());
+        addIntegerField("difficultyTree", Prompt.treeDifficulty());
+        addStringField("typeTree", Prompt.treeType());
     }
 
     @Override
     protected void execute() throws CommandException {
-        //FIXME implement command
+        try {
+            _receiver.registerTreeInHabitat(
+                    stringField("id"),
+                    stringField("idTree"),
+                    stringField("nameTree"),
+                    integerField("ageTree"),
+                    integerField("difficultyTree"),
+                    stringField("typeTree")
+            );
+        } catch (hva.exceptions.DuplicateTreeKeyException e) {
+            throw new DuplicateTreeKeyException(e.getKey());
+          
+        } catch (UnknownHabitatException e1) {
+            throw new UnknownHabitatKeyException(e1.getKey());
+        }
     }
 }
