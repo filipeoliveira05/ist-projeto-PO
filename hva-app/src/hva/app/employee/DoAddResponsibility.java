460f283187a87cfcaa6738d0a5c58c5619ee5096
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-18 20:12:48 +0100

    FILIPE - DoAddResponsibility primeira tentativa

--- hva-app/src/hva/app/employee/DoAddResponsibility.java
+++ hva-app/src/hva/app/employee/DoAddResponsibility.java
@@ -1,19 +1,28 @@
 package hva.app.employee;
 
-import java.text.Normalizer.Form;
+import hva.Hotel;
+
+import hva.app.exceptions.NoResponsibilityException;
+
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-import hva.Hotel;
-import hva.exceptions.NoResponsibilityException;
+
+import java.text.Normalizer.Form;
 
 class DoAddResponsibility extends Command<Hotel> {
 
     DoAddResponsibility(Hotel receiver) {
         super(Label.ADD_RESPONSABILITY, receiver);
+        addStringField("idEmployee", Prompt.employeeKey());
+        addStringField("idResponsability", Prompt.responsibilityKey());
     }
 
     @Override
     protected void execute() throws CommandException{
-        //FIXME implement command
+        try {
+            _receiver.addResponsibilityToEmployee(stringField("idEmployee"), stringField("idResponsability"));
+        } catch (hva.exceptions.NoResponsibilityException e) {
+            throw new NoResponsibilityException(e.getEmployeeKey(), e.getResponsabilityKey());
+        }
     }
 }
