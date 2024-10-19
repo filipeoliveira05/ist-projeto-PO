24b2fc49f779ddf53b3cce952cacba0b9d6de8b2
filipeoliveira05 <filipe.pinto.oliveira@tecnico.ulisboa.pt>
2024-10-19 21:33:16 +0100

    FILIPE - minor changes

--- hva-app/src/hva/app/habitat/DoShowAllTreesInHabitat.java
+++ hva-app/src/hva/app/habitat/DoShowAllTreesInHabitat.java
@@ -1,55 +1,48 @@
 package hva.app.habitat;
 
-import java.text.Normalizer.Form;
-
-import hva.tree.Tree;
-import java.util.Collection;
-import java.util.Collections;
+import hva.Hotel;
 import hva.habitat.Habitat;
-
+import hva.tree.Tree;
 
 import hva.app.exceptions.UnknownHabitatKeyException;
 
+import java.util.Collection;
+import java.util.Collections;
 
-import hva.Hotel;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
-//FIXME import other classes if needed
 
 class DoShowAllTreesInHabitat extends Command<Hotel> {
 
     DoShowAllTreesInHabitat(Hotel receiver) {
         super(Label.SHOW_TREES_IN_HABITAT, receiver);
-        //FIXME add command fields if needed
         addStringField("idHabitat", Prompt.habitatKey());
     }
 
     @Override
     protected void execute() throws CommandException {
-        //FIXME implement command
         String habitatId = stringField("idHabitat");
         Habitat habitat = _receiver.getHabitat(habitatId);
 
-        // Verifica se o habitat existe, caso contrário, lança a exceção apropriada
         if (habitat == null) {
             throw new UnknownHabitatKeyException(habitatId);
         }
 
-        // Obtém todas as árvores do habitat
         Collection<Tree> treesInHabitat = habitat.getAllTreesInHabitat();
 
-            if (!treesInHabitat.isEmpty()) {
-                for (Tree tree : treesInHabitat) {
-                    String treeInfo = "ÁRVORE|"
-                                    + tree.getId() + "|"
-                                    + tree.getName() + "|"
-                                    + tree.getAge() + "|"
-                                    + tree.getDifficulty() + "|"
-                                    + tree.getType() + "|"
-                                    + "GERARFOLHAS"; //FIXME
-                    _display.addLine(treeInfo);
-                }
+        if (!treesInHabitat.isEmpty()) {
+            for (Tree tree : treesInHabitat) {
+                String treeInfo = "ÁRVORE|"
+                                + tree.getId() + "|"
+                                + tree.getName() + "|"
+                                + tree.getAge() + "|"
+                                + tree.getDifficulty() + "|"
+                                + tree.getType() + "|"
+                                + "GERARFOLHAS"; //FIXME
+                _display.addLine(treeInfo);
             }
-
+        }
+        
+        _display.display();
     }
 }
