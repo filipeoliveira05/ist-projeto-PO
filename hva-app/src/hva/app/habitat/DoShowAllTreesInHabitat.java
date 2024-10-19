5651b25d7864c0eb98e39ac3890776e5ab492b07
Eduardo Fernandes <eduardo.x.fernandes@tecnico.ulisboa.pt>
2024-10-19 15:21:01 +0100

    EDU - DoShowAllTreesInHabitat de forma algo shameless, falta acertar CICLOBIOLOGICO

--- hva-app/src/hva/app/habitat/DoShowAllTreesInHabitat.java
+++ hva-app/src/hva/app/habitat/DoShowAllTreesInHabitat.java
@@ -2,6 +2,10 @@ package hva.app.habitat;
 
 import java.text.Normalizer.Form;
 
+import hva.tree.Tree;
+import java.util.Collection;
+import java.util.Collections;
+
 import hva.Hotel;
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
@@ -12,11 +16,28 @@ class DoShowAllTreesInHabitat extends Command<Hotel> {
     DoShowAllTreesInHabitat(Hotel receiver) {
         super(Label.SHOW_TREES_IN_HABITAT, receiver);
         //FIXME add command fields if needed
+        addStringField("idHabitat", Prompt.habitatKey());
     }
 
     @Override
     protected void execute() throws CommandException {
         //FIXME implement command
-    }
+        //String habitatId = stringField("idHabitat");
+        
+        Collection<Tree> treesInHabitat = _receiver.getHabitat(stringField("idHabitat")).getAllTreesInHabitat();
 
+            if (!treesInHabitat.isEmpty()) {
+                for (Tree tree : treesInHabitat) {
+                    String treeInfo = "ÁRVORE|"
+                                    + tree.getId() + "|"
+                                    + tree.getName() + "|"
+                                    + tree.getAge() + "|"
+                                    + tree.getDifficulty() + "|"
+                                    + tree.getType() + "|"
+                                    + "GERARFOLHAS"; //FIXME
+                    _display.addLine(treeInfo);
+                }
+            }
+
+    }
 }
