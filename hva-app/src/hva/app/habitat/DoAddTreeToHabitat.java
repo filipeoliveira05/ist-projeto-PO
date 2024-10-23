46ff407926639b988beeddcbd2b0ef6467a9f776
Eduardo Fernandes <eduardo.x.fernandes@tecnico.ulisboa.pt>
2024-10-23 16:34:10 +0100

    EDU - DoAddTreeToHabitat feito com loop no pedido de tipo

--- hva-app/src/hva/app/habitat/DoAddTreeToHabitat.java
+++ hva-app/src/hva/app/habitat/DoAddTreeToHabitat.java
@@ -1,18 +1,17 @@
 package hva.app.habitat;
 
+
 import hva.Hotel;
 import hva.exceptions.UnknownHabitatException;
-
 import hva.app.exceptions.DuplicateTreeKeyException;
 import hva.app.exceptions.UnknownHabitatKeyException;
-
 import pt.tecnico.uilib.menus.Command;
 import pt.tecnico.uilib.menus.CommandException;
+import pt.tecnico.uilib.forms.Form;
 
-import java.text.Normalizer.Form;
 
 class DoAddTreeToHabitat extends Command<Hotel> {
-    //FIXME PERENE/CADUCA únicos inputs válidos
+
     DoAddTreeToHabitat(Hotel receiver) {
         super(Label.ADD_TREE_TO_HABITAT, receiver);
         addStringField("id", Prompt.habitatKey());
@@ -20,38 +19,49 @@ class DoAddTreeToHabitat extends Command<Hotel> {
         addStringField("nameTree", Prompt.treeName());
         addIntegerField("ageTree", Prompt.treeAge());
         addIntegerField("difficultyTree", Prompt.treeDifficulty());
-        addStringField("typeTree", Prompt.treeType());
     }
 
     @Override
     protected void execute() throws CommandException {
-        //FIXME ciclo biológico
+        // Loop para garantir que a resposta seja "PERENE" ou "CADUCA"
+        String typeTree;
+        while (true) {
+            typeTree = Form.requestString(Prompt.treeType());
+            if (typeTree.equals("PERENE") || typeTree.equals("CADUCA")) {
+                break; // Sai do loop se o tipo for válido
+            }
+        }
+
         try {
+            // Tenta registrar a árvore no habitat
             _receiver.registerTreeInHabitat(
                     stringField("id"),
                     stringField("idTree"),
                     stringField("nameTree"),
                     integerField("ageTree"),
                     integerField("difficultyTree"),
-                    stringField("typeTree")
+                    typeTree
             );
 
+            // Exibição das informações da árvore registrada
             String treeInfo = "ÁRVORE|"
                             + stringField("idTree") + "|"
                             + stringField("nameTree") + "|"
                             + integerField("ageTree") + "|"
                             + integerField("difficultyTree") + "|"
-                            + stringField("typeTree") + "|"
-                            + "GERARFOLHAS";
+                            + typeTree + "|"
+                            + "GERARFOLHAS"; // FIXME: substituir "GERARFOLHAS" pela lógica correta do ciclo biológico
 
             _display.addLine(treeInfo);
             _display.display();
 
         } catch (hva.exceptions.DuplicateTreeKeyException e) {
             throw new DuplicateTreeKeyException(e.getKey());
-          
+
         } catch (UnknownHabitatException e1) {
             throw new UnknownHabitatKeyException(e1.getKey());
         }
     }
 }
+
+
